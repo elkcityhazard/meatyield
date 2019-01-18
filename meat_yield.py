dc6b0f69efbd2bf136320caadaa9d5ef59ef7eef
elkcityhazard <41174540+elkcityhazard@users.noreply.github.com>
2019-01-18 09:40:02 -0600

    meat_yield with updated notations in output
    added $ and % in the output.  Next I will work on formatting and error and exception handling.

--- meat_yield.py
+++ meat_yield.py
@@ -12,9 +12,8 @@ naked = float(naked)
 
 #establish the true cost - bag weight and purge
 naked_cost = (cost) / (naked/bagged)
-naked_cost = str(naked_cost)
 
-print("\nThe naked cost is: " + "\n\t" + naked_cost)
+print("\nThe naked cost is: " + "\n\t" + str("%.2f" %naked_cost))
 
 #enter the main cuts here
 
@@ -41,15 +40,18 @@ while add_active:
 
 print("\n --- Here are the results ---")
 
-for cut, weight in meat_dict.items():
-    print(cut.title() + " " + str(weight))
+for cut, meat_dict_values in meat_dict.items():
+    weight = meat_dict_values[0]
+    weight = float(weight)
+    print(cut.title() + " " + str("%.2f" %weight))
 
 
 #display the percentage yield
 print("\n\tThe percent of each cut is: ")
 for cut, meat_dict_values in meat_dict.items():
     percent = meat_dict_values[0] / (naked)
-    print(cut.title() + " " + str(percent))
+    percent= float(percent)
+    print(cut.title() + " " + str("%.2f" %percent) + "%")
 
 #create the total_cost list
 total_cost = []
@@ -57,9 +59,13 @@ total_cost = []
 print("\n\tThe cost of each cut is: ")
 for cut, meat_dict_values in meat_dict.items():
     cut_cost = meat_dict_values[0] * float(naked_cost)
+#append to total cost list
     total_cost.append(cut_cost)
-    print(cut.title() + " " + "$" + str(cut_cost))
-print("\n\tThe total cost of all cuts is: $" + str(sum(total_cost)))
+    cut_cost = float(cut_cost)
+    print(cut.title() + " " + "$" + str("%.2f" %cut_cost))
+    sum_total_cost = sum(total_cost)
+    sum_total_cost = float(sum_total_cost)
+print("\n\tThe total cost of all cuts is: $" + str("%.2f" %sum_total_cost))
 #create the total retail value list
 total_retail_value = []
 
@@ -68,8 +74,11 @@ print("\n\tThe retail dollar amount of each cut is: ")
 for cut, meat_dict_values in meat_dict.items():
     retail_dollar_amount = ((meat_dict_values[0]) * (meat_dict_values[1]))
     total_retail_value.append(retail_dollar_amount)
-    print(cut.title() + " " + "$" + str(retail_dollar_amount))
-print("\n\tThe total retail dollar amount is: $" + str(sum(total_retail_value)))
+    retail_dollar_amount = float(retail_dollar_amount)
+    print(cut.title() + " $" + str("%.2f" %retail_dollar_amount))
+    sum_total_retail_value = sum(total_retail_value)
+    sum_total_retail_value = float(sum_total_retail_value)
+print("\n\tThe total retail dollar amount is: $" + str("%.2f" %sum_total_retail_value))
 #display margin of cut
 print("\n\tThe margin of the cuts in an ideal environment are: ")
 for cut, meat_dict_values in meat_dict.items():
@@ -77,7 +86,7 @@ for cut, meat_dict_values in meat_dict.items():
     retail_dollar_amount = meat_dict_values[0] * meat_dict_values[1]
     profit = float(retail_dollar_amount) - float(cut_cost)
     margin = profit / retail_dollar_amount
-    print(str(margin) + " %")
+    print(str("%.2f" %margin) + " %")
 
 #create the sum_weight list
 sum_weight = []
@@ -86,16 +95,17 @@ print("\n\tThe amount of waste generated from the yield is: ")
 for cut, meat_dict_values in meat_dict.items():
     sum_weight.append(meat_dict_values[0])
 waste_generated = naked - sum(sum_weight)
-print(str(waste_generated) + " lbs.")
+print(str("%.2f" %waste_generated) + " lbs.")
 waste_percentage = waste_generated / naked
 waste_percentage_cost = float(waste_generated) * float(naked_cost)
-print("\nThe waste percentage is: " + str(waste_percentage))
-print("\tThat will add: " + str(waste_percentage_cost) + " to your unrecorded shrink.")
+print("\nThe waste percentage is: " + str("%.2f" %waste_percentage) + "%")
+print("\tThat will add: $" + str("%.2f" %waste_percentage_cost) + " to your unrecorded shrink.")
 
 gross_profit = sum(total_retail_value) - sum(total_cost)
+gross_profit = float(gross_profit)
 
-print("\n\tThe total gross profit is: $" + str(gross_profit))
+print("\n\tThe total gross profit is: $" + str("%.2f" %gross_profit))
 
 total_margin = gross_profit / sum(total_retail_value)
 
-print("\n\tThe overall margin of all available cuts is: %" + str(total_margin))
+print("\n\tThe blended margin of all available cuts is: " + str("%.2f" %total_margin) + "%")
